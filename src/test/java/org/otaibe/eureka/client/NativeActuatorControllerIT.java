1d1687afc439ba1a11e2e51c52f869aac688c3be
Triphon Penakov <triphon@gmail.com>
2019-10-10 11:31:30 +0300

    Actuator controller native test

---
+++ src/test/java/org/otaibe/eureka/client/NativeActuatorControllerIT.java
@@ -0,0 +1,9 @@
+package org.otaibe.eureka.client;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeActuatorControllerIT extends ActuatorControllerTest {
+
+    // Execute the same tests but in native mode.
+}
